9791200ea1c4d5adfea8e392a8605a00661fe050
Jessica Watts <jessperr182@hotmail.com>
2020-09-11 23:36:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,26 +2,10 @@ def my_collect(array)
   i = 0
     while i < array.length
     
-    
     i += 1
   end   
   result
 end  
-
-
-
-
-
-
-def my_collect(array)
-  i = 0
-  result = []
-  while i < array.length
-    result.push(yield array[i])
-      i += 1
-  end
-  result
-end
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
