961a5b700182c1e0eae0f05f1b06fcb836c0b8e2
Jessica Watts <jessperr182@hotmail.com>
2020-09-11 23:35:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,29 @@
 def my_collect(array)
   i = 0
-  array.each {|name| puts x}
-    while yield
+    while i < array.length
     
+    
+    i += 1
+  end   
+  result
+end  
 
+
+
+
+
+
+def my_collect(array)
+  i = 0
+  result = []
+  while i < array.length
+    result.push(yield array[i])
+      i += 1
+  end
+  result
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
